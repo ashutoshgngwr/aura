6790ea38984ea388b20b7a3ef89e3b0019af5d10
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2020-08-17 00:29:43 +0530

    enable auto importing trusted GPG keys

--- build-packages.sh
+++ build-packages.sh
@@ -8,7 +8,8 @@ RUN_INTERVAL="86400"
 # output directory for built packages. used in build_package()
 OUTPUT_DIR="$(pwd)/output"
 
-# a file containing AUR packages to build. 1 name per line
+# a file containing AUR packages to build. 1 name per line optionally with GPG keys it needs to import
+# e.g. "spotify 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90 2EBF997C15BDA244B6EBF5D84773BD5E130D1D45"
 PACKAGE_LIST="$(pwd)/packagelist"
 
 # sets up a non-root user to 'makepkg' script. also gives the newly
@@ -19,6 +20,15 @@ setup_build_user() {
   test -d "/etc/sudoers.d" || mkdir "/etc/sudoers.d"
   echo "build ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/build
   setfacl -m u:build:rwx . # permissions on current dir and stuff
+
+  # fix: dirmngr is giving TLS authentication errors when importing keys.
+  # happening because the default keyservers' CA is distributed with the
+  # binaries and its not present in the common CA store?
+  test -d "/home/build/.gnupg" || mkdir "/home/build/.gnupg"
+  echo "hkp-cacert /usr/share/gnupg/sks-keyservers.netCA.pem" >> /home/build/.gnupg/dirmngr.conf
+  chown -R build "/home/build/.gnupg"
+  chmod 700 "/home/build/.gnupg"
+  chmod 600 "/home/build/.gnupg/dirmngr.conf"
 }
 
 # installs any prerequisites required for the
@@ -51,6 +61,7 @@ check_updates() {
 # accepts following position args
 # 1. packge name: name of the AUR package being built
 build_package() {
+  test -z "$keys" || sudo -u build gpg --receive-keys $keys
   sudo -u build git clone --depth=1 "https://aur.archlinux.org/$1.git"
   cd "$1"
   sudo -u build makepkg -s --noconfirm --noprogressbar PKGDEST="$OUTPUT_DIR"
@@ -65,8 +76,8 @@ main() {
     exit 0
   fi
 
-  while read package; do
-    build_package "$package"
+  while read package keys; do
+    build_package "$package" "$keys"
   done < "$PACKAGE_LIST"
   repo-add "$OUTPUT_DIR/aura.db.tar.gz" $OUTPUT_DIR/*.pkg.tar.zst
 }
