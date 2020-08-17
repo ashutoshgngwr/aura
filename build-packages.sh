91835fec57946e1b009cc2a96fbd4caf412c5efa
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2020-08-17 17:10:10 +0530

    add gpg signature generation

--- build-packages.sh
+++ build-packages.sh
@@ -17,30 +17,51 @@ PACKAGE_LIST="$(pwd)/packagelist"
 setup_build_user() {
   useradd -m build
   gpasswd -a build wheel
-  test -d "/etc/sudoers.d" || mkdir "/etc/sudoers.d"
+  test -d "/etc/sudoers.d" || mkdir -p "/etc/sudoers.d"
   echo "build ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/build
   setfacl -m u:build:rwx . # permissions on current dir and stuff
 
   # fix: dirmngr is giving TLS authentication errors when importing keys.
   # happening because the default keyservers' CA is distributed with the
   # binaries and its not present in the common CA store?
-  test -d "/home/build/.gnupg" || mkdir "/home/build/.gnupg"
+  test -d "/home/build/.gnupg" || mkdir -p "/home/build/.gnupg"
   echo "hkp-cacert /usr/share/gnupg/sks-keyservers.netCA.pem" >> /home/build/.gnupg/dirmngr.conf
   chown -R build "/home/build/.gnupg"
   chmod 700 "/home/build/.gnupg"
   chmod 600 "/home/build/.gnupg/dirmngr.conf"
 }
 
-# installs any prerequisites required for the
-# build process.
+# sets up the private PGP key passed using the environment variable. This key is used for
+# generating package and repo db signatures. A few GPG conf params are needed to be set to
+# pass passphrase via stdin. https://stackoverflow.com/a/59170001/2410641
+setup_signing_key() {
+  set +x
+  echo -e "$PGP_SECRET_KEY" | gpg --import --batch
+  set -x
+
+  echo "use-agent" >> "$HOME/.gnupg/gpg.conf"
+  echo "pinentry-mode loopback" >> "$HOME/.gnupg/gpg.conf"
+  echo "allow-loopback-pinentry" >> "$HOME/.gnupg/gpg-agent.conf"
+}
+
+# generates a detached signature for the given file. Accepts the following positional args
+# 1. file path: path of the file to sign
+sign_file() {
+  set +x
+  echo "Signing file $1..."
+  echo "$PGP_SECRET_KEY_PASSPHRASE" | gpg --batch --passphrase-fd 0 --detach-sign --no-armor -u "$PGP_KEY_ID" "$1"
+  set -x
+}
+
+# installs any prerequisites required for the build process.
 install_prerequisites() {
   setup_build_user
   pacman -Sqyuu --noconfirm --noprogressbar base-devel git jq
+  setup_signing_key
 }
 
-# checks if any PKGBUILDs have been modified since this script was
-# last run. This is determined based on the  schedule provided by
-# the 'RUN_INTERVAL' variable
+# checks if any PKGBUILDs have been modified since this script was last run. This is determined
+# based on the schedule provided by the 'RUN_INTERVAL' variable
 check_updates() {
   BASE_URL="https://aur.archlinux.org/rpc/?v=5&type=info"
   ARGS=""
@@ -58,16 +79,35 @@ check_updates() {
   fi
 }
 
-# builds a given package using 'makepkg'.
-# accepts following position args
+# builds a given package using 'makepkg'. accepts the following position args
 # 1. packge name: name of the AUR package being built
+# 2. keys: a space seperate list of PGP keys to import for verifying source
+#    signatures during the build
 build_package() {
   test -z "$keys" || sudo -u build gpg --receive-keys $keys
   sudo -u build git clone --depth=1 "https://aur.archlinux.org/$1.git"
   cd "$1"
   sudo -u build makepkg -s --noconfirm --noprogressbar PKGDEST="$OUTPUT_DIR"
+  sign_file $(ls "$OUTPUT_DIR/$1"-*.pkg.tar.zst)
   cd ..
   rm -rf "$1"
+}
+
+# build_repo_db runs the repo-add script to generate a new db adding all the packages
+# present in the 'OUTPUT_DIR'. It also signs the resulting DB files using 'sign_file'
+# while preserving the file structure mentioned in the following wiki.
+# https://wiki.archlinux.org/index.php/DeveloperWiki:Repo_DB_Signing#[RFC]_Repo_DB_signing_(and_ISO's/other_artefacts)
+build_repo_db() {
+  REPO_BASE="$OUTPUT_DIR/aura"
+  REPO_DB_BASE="$REPO_BASE.db"
+  REPO_DB="$REPO_DB_BASE.tar.gz"
+  REPO_FILES_BASE="$REPO_BASE.files"
+  REPO_FILES="$REPO_BASE.files.tar.gz"
+  repo-add "$REPO_DB" $OUTPUT_DIR/*.pkg.tar.zst
+  sign_file "$REPO_DB"
+  sign_file "$REPO_FILES"
+  ln -rs "$REPO_DB.sig" "$REPO_DB_BASE.sig"
+  ln -rs "$REPO_FILES.sig" "$REPO_FILES_BASE.sig"
 }
 
 main() {
@@ -76,7 +116,7 @@ main() {
   while read package keys; do
     build_package "$package" "$keys"
   done < "$PACKAGE_LIST"
-  repo-add "$OUTPUT_DIR/aura.db.tar.gz" $OUTPUT_DIR/*.pkg.tar.zst
+  build_repo_db
 }
 
 main "$@"
